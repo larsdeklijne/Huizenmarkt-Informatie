eaf342b9f347f12262b21f1faf43b0052775fa3f
1105799 <larsdeklijne@gmail.com>
2023-04-18 00:46:00 +0200

    html layout1 working

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,76 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
 function Home() {
   return (
-    <Container fluid className="bg-light p-5">
-    <Row className="justify-content-center">
-      <Col lg={6} md={8}>
-        <h1 className="text-center">ReformedDegen</h1>
-        <p className="lead text-center">From degenerate trades to informed investments</p>
-        <hr />
-        <p>Our website aims to transform visitors from "degenerate traders" into informed investors through educational content, market analysis, and portfolio management tools.</p>
-        <div className="text-center">
-          <Button variant="primary" className="mr-3">Learn More</Button>
-          <Button variant="outline-primary">Sign Up</Button>
-        </div>
-      </Col>
-    </Row>
-  </Container>
+    <div>
+    <div >
+      <div class="fullscreen-bg bg-dark p-5"><div class="content">
+        <Row className="justify-content-center">
+          <Col lg={6} md={8}>
+            <h1 className="text-center">Huizenmarkt Nederland</h1>
+            <hr />
+          </Col>
+        </Row>
+      </div></div>
+  </div>
+  <Container style={{ backgroundColor: 'grey', color: 'white', padding: '2rem', borderRadius: '10px', marginTop: '50px' }}>
+    <br></br>
+      <h1 style={{ color: 'black', textAlign: 'center' }}><b>Welkom bij onze website!</b></h1>
+    <br></br>
+      <p style={{ textAlign: 'justify' }}>Ontdek actuele informatie over hypotheekrentes, huizenprijzen en uw maximale leencapaciteit in Nederland.</p>
+      <p style={{ textAlign: 'justify' }}>Bereken uw hypotheekrente en leencapaciteit op basis van uw persoonlijke situatie en volg de actuele huizenprijzen en prijsontwikkelingen in uw regio.</p>
+      <p style={{ textAlign: 'justify' }}>Blijf op de hoogte van de ontwikkelingen op de huizenmarkt en neem de beste financiële beslissingen voor uw situatie.</p>
+    </Container>
+
+<br></br>
+<br></br>
+<br></br>
+
+    <Container>
+    <h1 style={{ color: 'black', textAlign: 'center' }}><b>Onze paginas</b></h1>
+    <br></br>
+      <Row className="justify-content-center">
+        <Col md={4}>
+          <Card>
+            <Card.Img variant="top" src="https://via.placeholder.com/286x180" />
+            <Card.Body>
+              <Card.Title>Hypotheekrentes</Card.Title>
+              <Card.Text>
+                Ontdek actuele hypotheekrentes en vergelijk verschillende aanbieders.
+              </Card.Text>
+              <Button variant="primary" href="/hypotheekrentes">Meer informatie</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <Card.Img variant="top" src="https://via.placeholder.com/286x180" />
+            <Card.Body>
+              <Card.Title>Huizenprijzen</Card.Title>
+              <Card.Text>
+                Volg de actuele huizenprijzen en prijsontwikkelingen in uw regio.
+              </Card.Text>
+              <Button variant="primary" href="/huizenprijzen">Meer informatie</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <Card.Img variant="top" src="https://via.placeholder.com/286x180" />
+            <Card.Body>
+              <Card.Title>Leencapaciteit</Card.Title>
+              <Card.Text>
+                Bereken uw maximale leencapaciteit op basis van uw persoonlijke situatie.
+              </Card.Text>
+              <Button variant="primary" href="/leencapaciteit">Meer informatie</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+
+</div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
-
 function Header() {
   return (
     <Navbar  expand="lg"  className="navbar-custom">
     <Container>
       <Navbar.Brand href="/" className="text-white">
-        Reformed Degen
+        Huizenmarkt waanzin
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
       <Nav className="ml-auto">
         <div className="nav-link-container">
-            <Nav.Link href="#markets" className="text-white">Markets</Nav.Link>
+            <Nav.Link href="#markets" className="text-white">Hypotheekrentes</Nav.Link>
         </div>
         <div className="nav-link-container">
-            <Nav.Link href="#marketanalysis" className="text-white">Market Analysis</Nav.Link>
+            <Nav.Link href="#marketanalysis" className="text-white">Huizenprijzen</Nav.Link>
         </div>
         <div className="nav-link-container">
-            <Nav.Link href="#strategies" className="text-white">Strategies</Nav.Link>
-        </div>
-        <div className="nav-link-container">
-            <Nav.Link href="#portfolio" className="text-white">Portfolio</Nav.Link>
+            <Nav.Link href="#strategies" className="text-white">Leencapaciteit</Nav.Link>
         </div>
         </Nav>
       </Navbar.Collapse>
